38e3f6e7ee32ab76c2feb2f74cc40145b471d458
John O'Donnell <johnodonnell1997@icloud.com>
2023-06-16 12:31:04 -0500

    added import

---
+++ target/release/calculator.d
@@ -0,0 +1,1 @@
+/home/lvcky/rust/calculator/target/release/calculator: /home/lvcky/rust/calculator/src/main.rs /home/lvcky/rust/calculator/src/operators.rs
---
+++ target/release/deps/calculator-9b697b4fcdee4bc8.d
@@ -0,0 +1,6 @@
+/home/lvcky/rust/calculator/target/release/deps/calculator-9b697b4fcdee4bc8: src/main.rs src/operators.rs
+
+/home/lvcky/rust/calculator/target/release/deps/calculator-9b697b4fcdee4bc8.d: src/main.rs src/operators.rs
+
+src/main.rs:
+src/operators.rs:
